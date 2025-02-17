7c223297c2c05729699d784e2125b92a54e20d56
Nehal409 <nehalnasir619@gmail.com>
2025-02-17 12:45:27 +0500

    implement card container

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({
   searchParams,
@@ -7,6 +8,18 @@ export default async function Home({
 }) {
   // Robust server side form handling
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 100,
+      author: { _id: 1, name: "John Doe" },
+      _id: 1,
+      description: "A new way to connect with people",
+      image: "/house-of-zen-logo-removebg-preview.png",
+      category: "Tech",
+      title: "Startup 1",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -18,6 +31,20 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and Get Notified in Virtual Competition
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
