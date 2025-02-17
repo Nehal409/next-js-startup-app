2efd6ed1cbb11c6373a68a5d51daf507c014c0de
Nehal409 <nehalnasir619@gmail.com>
2025-02-17 11:38:19 +0500

    implement search form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,12 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  // Robust server side form handling
+  const query = (await searchParams).query;
   return (
     <>
       <section className="pink_container">
@@ -9,6 +17,7 @@ export default function Home() {
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Notified in Virtual Competition
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
