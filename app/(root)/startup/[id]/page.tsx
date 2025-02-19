43153dd883ef477dc43d9c279542368cce7c8c1e
Nehal409 <nehalnasir619@gmail.com>
2025-02-19 12:34:54 +0500

    update next config file

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,9 +1,12 @@
-import { StartupTypeCard } from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import {
+  PLAYLIST_BY_SLUG_QUERY,
+  STARTUP_BY_ID_QUERY,
+} from "@/sanity/lib/queries";
 import markdownIt from "markdown-it";
 import Image from "next/image";
 import Link from "next/link";
@@ -14,7 +17,14 @@ const md = markdownIt();
 
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
-  const post: StartupTypeCard = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  // Makes two independent requests to fetch the post and editor picks concurrently/parallel
+  const [post, { select: editorPosts }] = await Promise.all([
+    await client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks-new",
+    }),
+  ]);
 
   if (!post) return notFound();
 
@@ -71,13 +81,21 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {/* TODO: Recommended */}
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, index: number) => (
+                <StartupCard key={index} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
 
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
-          <View id={id}/>
+          <View id={id} />
         </Suspense>
       </section>
-
     </>
   );
 };
